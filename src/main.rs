9f59f6cdb28918b46bcc351256595996df7bb1a2
Kalidass Mookkaiah <kalidass@gmail.com>
2023-05-03 23:20:50 +0100

    copy from another instance

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ struct User {
     sign_in_count: u64
 }
 fn main() {
+    // create struct instance
     let user1 = User{
         active: true,
         username: String::from("km"),
@@ -15,6 +16,7 @@ fn main() {
     // can not update as not mutable
     //user1.email = String::from("km1@example.com");
 
+    // create mutable instance
     let mut user2 = User{
         active: true,
         username: String::from("km"),
@@ -24,6 +26,21 @@ fn main() {
 
     // either full mutable or nothing mutable
     user2.email = String::from("km1@example.com");
+
+    // create instance from another instance values
+    let user3 = User{
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count
+    };
+
+    // shorthand copy
+    let user4 = User{
+        email: String::from("123@example.com"),
+        ..user2
+    };
+
 }
 
 fn build_user(email: String, username: String) -> User {
