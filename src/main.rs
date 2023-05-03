bdced58dae74ab3fa775987d3fb30a3513b31925
Kalidass Mookkaiah <kalidass@gmail.com>
2023-05-03 23:13:41 +0100

    create struct mutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64
+}
 fn main() {
-    println!("Hello, world!");
+    let user1 = User{
+        active: true,
+        username: String::from("km"),
+        email: String::from("km@example.com"),
+        sign_in_count: 1
+    };
+
+    // can not update as not mutable
+    //user1.email = String::from("km1@example.com");
+
+    let mut user2 = User{
+        active: true,
+        username: String::from("km"),
+        email: String::from("km@example.com"),
+        sign_in_count: 1
+    };
+
+    // either full mutable or nothing mutable
+    user2.email = String::from("km1@example.com");
 }
